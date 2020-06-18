f1406472519d03c37a52713ae06ed76dc82dd6d6
Dmitry <free-wind-2012@yandex.ru>
2020-06-18 12:36:10 +0300

    Незначительные изменения

--- server/src/main/java/ServerMain.java
+++ server/src/main/java/ServerMain.java
@@ -10,22 +10,26 @@ public class ServerMain {
     public ServerMain() {
         ServerSocket server = null;
         Socket socket = null;
-        Scanner scanner;
         HibernateUtil hibernateUtil;
 
         try {
-            scanner = new Scanner(System.in);
             server = new ServerSocket(8189);
             hibernateUtil = new HibernateUtil();
 
 
             Thread t1 = new Thread(() -> {
-                System.out.println("Для выхода введите /exit");
+                System.out.println("Для выхода введите /exit для справки help");
+                Scanner scanner = new Scanner(System.in);
                 while (true) {
                     String str = scanner.nextLine();
                     if (str.equals("/exit")) {
                         System.exit(0);
                         break;
+                    }
+
+                    if (str.contains("help")) {
+                        System.out.println("Справка по командам:");
+                        System.out.println("/exit - выход из программы");
                     }
                 }
             });
